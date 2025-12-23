88f449309f37f4fdc0a5d3e11c441e52e535c44d
acedMaxxx <nimitak@gmail.com>
2025-12-23 13:37:51 +0300

    Create config.tcl

---
+++ src/config.tcl
@@ -0,0 +1,8 @@
+# Target 100MHz (10.0ns period)
+set ::env(CLOCK_PERIOD) "10.0"
+
+# Focus on speed (Delay) over Area
+set ::env(SYNTH_STRATEGY) "DELAY 0"
+
+# Don't pack the gates too tightly to allow faster routing
+set ::env(PL_TARGET_DENSITY) 0.45
